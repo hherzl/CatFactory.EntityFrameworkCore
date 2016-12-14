fe3d0dfef57bfd230256ed7f530209e046a2bd40
HH <hans.herzl@live.com>
2016-12-14 03:41:09 -0500

    Version 1.0.0-alpha-build05: addition of overload for EntityClassDefinition

--- src/CatFactory.EfCore/EntityClassDefinition.cs
+++ src/CatFactory.EfCore/EntityClassDefinition.cs
@@ -6,19 +6,37 @@ namespace CatFactory.EfCore
 {
     public class EntityClassDefinition : CSharpClassDefinition
     {
-        public EntityClassDefinition(IDbObject dbObject)
+        public EntityClassDefinition(ITable table)
         {
             Namespaces.Add("System");
 
-            Name = dbObject.GetSingularName();
+            Name = table.GetSingularName();
 
             Constructors.Add(new ClassConstructorDefinition());
 
             var resolver = new ClrTypeResolver() as ITypeResolver;
 
-            for (var i = 0; i < dbObject.Columns.Count; i++)
+            for (var i = 0; i < table.Columns.Count; i++)
             {
-                var column = dbObject.Columns[i];
+                var column = table.Columns[i];
+
+                Properties.Add(new PropertyDefinition(resolver.Resolve(column.Type), column.GetPropertyName()));
+            }
+        }
+
+        public EntityClassDefinition(IView view)
+        {
+            Namespaces.Add("System");
+
+            Name = view.GetSingularName();
+
+            Constructors.Add(new ClassConstructorDefinition());
+
+            var resolver = new ClrTypeResolver() as ITypeResolver;
+
+            for (var i = 0; i < view.Columns.Count; i++)
+            {
+                var column = view.Columns[i];
 
                 Properties.Add(new PropertyDefinition(resolver.Resolve(column.Type), column.GetPropertyName()));
             }
